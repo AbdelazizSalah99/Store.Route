ec2adf811e8e242de4ed319d0773816b4db77c1c
abdelazizsalah99 <abdelaziz.salah99@gmail.com>
2025-05-18 14:57:29 +0300

    Add project files.

---
+++ Infrastructure/Presentation/ProductsController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Abstractions;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController(IServiceManager serviceManager) : ControllerBase
+    {
+        // end point : public non static method
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var result = await serviceManager.ProductService.GetAllProductsAsync();
+            if (result is null) return BadRequest(); // 400
+            return Ok(result); // 200
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductById(int id)
+        {
+            var result = await serviceManager.ProductService.GetProductByIdAsync(id);
+            if (result is null) return NotFound(); // 404
+            return Ok(result); // 200
+
+        }
+
+        //[HttpGet]
+        //public async Task<IActionResult> GetAllBrands()
+        //{
+        //    var result = await serviceManager.ProductService.GetAllBrandsAsync();
+        //    if (result is null) return BadRequest(); // 400
+        //    return Ok(result); // 200
+        //}
+        //[HttpGet]
+        //public async Task<IActionResult> GetAllTypes()
+        //{
+        //    var result = await serviceManager.ProductService.GetAllTypesAsync();
+        //    if (result is null) return BadRequest(); // 400
+        //    return Ok(result); // 200
+        //}
+
+
+
+    }
+}
